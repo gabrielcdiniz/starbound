2c48e5e702772e66a1632c77b6cff8c57666279f
gabrielcdiniz <gabriel.dinizcd1997@gmail.com>
2024-11-21 08:48:16 -0300

    refactor(hooks): remove unnecessary function

--- src/hooks/lists/hook.ts
+++ src/hooks/lists/hook.ts
@@ -4,8 +4,6 @@ import type { List } from '@prisma/client';
 
 import { getLastLists } from '@/actions/lists';
 
-const delay = (ms: number) => new Promise((resolve) => setTimeout(resolve, ms));
-
 export function useLists() {
   const [isPendingLastLists, startTransitionLastLists] = useTransition();
 
@@ -13,8 +11,6 @@ export function useLists() {
 
   const fetchLastLists = useCallback(() => {
     startTransitionLastLists(async () => {
-      await delay(5000);
-
       const lists = await getLastLists();
 
       if (!lists.error) {
